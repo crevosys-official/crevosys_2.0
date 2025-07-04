17b66013817a5d1b9f6ccdffc7697b2d08964755
sahidDev09 <iamsahidofficial99@gmail.com>
2025-07-04 11:55:57 +0600

    customized hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import Image from "next/image";
 import React from "react";
 
 const page = () => {
   return (
-    <div className="bg-gradient-to-t from-[#070707] to-[#221f35] min-h-screen">
+    <div
+      className={[
+        "min-h-screen",
+        "bg-gradient-to-t from-[#070707] to-[#221f35]",
+      ].join(" ")}>
+      <div
+        className={[
+          "fixed inset-0 z-0 pointer-events-none",
+          "flex items-center justify-center w-full h-full",
+        ].join(" ")}>
+        <Image
+          src="/gradient.png"
+          alt="gradient background"
+          layout="fill"
+          objectFit="cover"
+          className=" opacity-10"
+          priority
+        />
+      </div>
       <Navbar />
       <Hero />
     </div>
