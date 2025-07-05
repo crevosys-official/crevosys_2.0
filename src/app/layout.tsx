84337fb01ff10de32983b519ea01d3750e00ba13
sahidDev09 <iamsahidofficial99@gmail.com>
2025-07-05 16:19:32 +0600

    added lenis smooth scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { ReactLenis } from "@/lib/lenis";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,11 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={` antialiased`}
-      >
-        {children}
-      </body>
+      <ReactLenis root>
+        <body className={` antialiased`}>{children}</body>
+      </ReactLenis>
     </html>
   );
 }
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -2,6 +2,7 @@
 import { MoveRight, Star } from "lucide-react";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import PixelCard from "./animation/PlexCard";
 
 // Define the type for a service card
 type ServiceCard = {
@@ -21,21 +22,21 @@ const Services = () => {
   }, []);
 
   return (
-    <div className="relative overflow-hidden services md:pb-20 w-full mt-20 rounded-2xl">
+    <div className="relative overflow-hidden services md:pb-20 w-full mt-10 pt-10 rounded-2xl">
       {/* Background with opacity */}
       <div
-        className="absolute inset-0 bg-cover bg-no-repeat bg-center opacity-50"
+        className="absolute inset-0 bg-cover bg-no-repeat bg-center opacity-60"
         style={{ backgroundImage: "url('/elements/servicebg.png')" }}></div>
 
       {/* Content */}
       <div className="relative z-10">
-        <div className=" flex flex-col gap-3 mt-4">
+        <div className=" flex flex-col gap-4 mt-4">
           <div className=" border-gray-600 border w-fit flex justify-center mx-auto px-4 py-1.5  rounded-full gap-2 items-center text-zinc-300">
             <Star className=" w-3" />
             Services
           </div>
 
-          <h1 className="text-4xl font-semibold font-heading tracking-wide text-center text-zinc-200">
+          <h1 className="text-4xl font-heading tracking-wide text-center text-zinc-200">
             Our Solutions for your Digital Growth
           </h1>
           <p className=" w-1/2 text-md tracking-wide text-gray-400 text-center flex mx-auto">
@@ -47,27 +48,27 @@ const Services = () => {
 
         <div className="container mx-auto grid md:grid-cols-4 md:gap-7 gap-5 px-4 md:px-0 mb-10 md:mb-0">
           {service.map((serviceCard, index) => (
-            <div
-              key={index}
-              className=" group bg-[#1B1B1B] p-10 flex flex-col gap-5 rounded-2xl md:my-10 hover:border border-gray-600 transition-all arrow-hover:text-orange-500">
-              <Image
-                className=" md:w-32 md:h-32 w-24 h-24"
-                height={100}
-                width={100}
-                src={serviceCard.icon}
-                alt=""
-              />
-              <h1 className=" text-3xl text-white">{serviceCard.title}</h1>
-              <p className=" text-gray-500 text-lg">
-                {serviceCard.description}
-              </p>
-              <p className=" flex gap-3 hover:text-blue-500 items-center group-hover: text-white">
-                Learn more
-                <span className="group-hover:text-orange-500 group-hover:animate-bounce">
-                  <MoveRight />
-                </span>
-              </p>
-            </div>
+            <PixelCard key={index} variant="blue" className="md:my-10">
+              <div className="absolute inset-0 p-10 flex flex-col gap-5">
+                <Image
+                  className="md:w-32 md:h-32 w-24 h-24"
+                  height={100}
+                  width={100}
+                  src={serviceCard.icon}
+                  alt=""
+                />
+                <h1 className="text-3xl text-white">{serviceCard.title}</h1>
+                <p className="text-gray-500 text-lg">
+                  {serviceCard.description}
+                </p>
+                <p className="flex gap-3 hover:text-blue-500 items-center group-hover:text-white">
+                  Learn more
+                  <span className="group-hover:text-orange-500 group-hover:animate-bounce">
+                    <MoveRight />
+                  </span>
+                </p>
+              </div>
+            </PixelCard>
           ))}
         </div>
         <Image
